c92c3b7dae7a89863c0755406f0d5250f0d87f53
RafaelCruz <rafael.cruz@germinare.org.br>
2025-10-29 10:05:58 -0300

    Mudando o Deploy para Render

--- app.py
+++ app.py
@@ -207,8 +207,3 @@ def chat():
     except Exception as e:
         print(f"Erro ao consumir a API: {e}")
         return jsonify({"error": "Erro ao processar a solicitação."}), 500
-
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
